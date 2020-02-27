390cdf51cda4456963783123a5573636accebfdc
stefanija.ristomanova <stefanija.ristomanova>
2020-02-27 17:36:17 +0100

    Added post method

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-// create express app
+const db = require('./config/db.config');
+
 const app = express();
-// Setup server port
-const port = process.env.PORT || 4000;
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-// define a root/default route
-app.get('/', (req, res) => {
-    res.json({"message": "Hello World"});
-});
-// listen for requests
-app.listen(port, () => {
-    console.log(`Node server is listening on port ${port}`);
-});
+
+const port = 4000;
+app.use(bodyParser.urlencoded({extended:true }))
+
+
+MongoClient.connect(db.url, (err,database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app,database);
+    app.listen(port, () => {
+        console.log('livee')
+    })
+
+})
